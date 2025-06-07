97503b94c98c52e681fac0bd75e0e50db014f00e
AxlER8R <axl@axler8r.io>
2025-06-08 10:40:20 +1200

    env: bootstrap the project

---
+++ application/ficher/main.py
@@ -0,0 +1,7 @@
+def main():
+  print("Hello from ficher!")
+
+
+if __name__ == "__main__":
+  main()
+
---
+++ application/historian/main.py
@@ -0,0 +1,7 @@
+def main():
+  print("Hello from historian!")
+
+
+if __name__ == "__main__":
+  main()
+
